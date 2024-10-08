48b2abbac52a25e1948d1324fbd2a744e048c4ce
Anubhav Dinkar <ad6641@nyu.edu>
2024-10-08 17:44:33 -0400

    completed signup page styling and functionality; added user model

--- checkpoint/login/models.py
+++ checkpoint/login/models.py
@@ -1,2 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+
+class CheckpointUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'  # Use email as the username
+    REQUIRED_FIELDS = ['username']  # Require username in addition to email
--- checkpoint/login/views.py
+++ checkpoint/login/views.py
@@ -2,18 +2,44 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 
-# Signup view
+
+User = get_user_model()
+
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # TODO: redirect to setup profile page
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+        email = request.POST['email']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        # Basic validation
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists.")
+            return redirect('signup')
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+            return redirect('signup')
+    
+        if password1!=password2:
+            messages.error(request, "Passwords don't match.")
+            return redirect('signup')
+    
+        
+        # Create new user
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password1
+        )
+        user.save()
+
+        messages.success(request, "New user added!")
+        # TODO: redirect to profile page
+
+    return render(request, 'signup.html')
 
 def login_view(request):
     if request.method == 'POST':
