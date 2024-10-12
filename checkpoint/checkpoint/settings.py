cee44065152db1a376d96cb64672bb1cc62f0842
Zhuo ZENG <zzengan@connect.ust.hk>
2024-10-12 15:46:00 -0400

    fix img upload bug

--- checkpoint/checkpoint/settings.py
+++ checkpoint/checkpoint/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
 STATIC_ROOT = 'static'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
