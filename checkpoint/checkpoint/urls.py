4aa47b08af37802230048bb1103a721fdfed0a6d
Anubhav Dinkar <ad6641@nyu.edu>
2024-10-13 17:37:41 -0400

    modified URLs to serve media files

--- checkpoint/checkpoint/urls.py
+++ checkpoint/checkpoint/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import re_path
+from django.views.static import serve 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('profile/', include('userProfile.urls')),
     path('create-profile/', include('createUserProfile.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
