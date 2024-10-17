a03bdd9e5addeae6096784dff31626ba7b4be57c
Anubhav Dinkar <ad6641@nyu.edu>
2024-10-17 14:07:27 -0400

    added search result page

--- checkpoint/gamesearch/views.py
+++ checkpoint/gamesearch/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponseRedirect
 import requests
 import os
 import json
+from datetime import datetime
 
 def search_game(request):
     def authorize_igdb():
         url = f"https://id.twitch.tv/oauth2/token?client_id={os.environ.get("client_id")}&client_secret={os.environ.get("client_secret")}&grant_type=client_credentials"
+
         return requests.request("POST", url, headers={}, data={})
 
     if request.method == 'POST':
@@ -15,8 +17,8 @@ def search_game(request):
         
 
         url = "https://api.igdb.com/v4/games"
-
-        payload = "search \"%s\"; fields *;\n" % (game_query)
+        
+        payload = "search \"%s\"; fields name, first_release_date, cover, genres;\n" % (game_query)
         headers = {
         'Client-ID': os.environ.get("client_id"),
         'Authorization': f'Bearer {auth.json()["access_token"]}',
@@ -25,8 +27,45 @@ def search_game(request):
 
         response = requests.request("POST", url, headers=headers, data=payload)
 
-        search_result = response.json()
-        return render(request, 'search_result.html', {'games': search_result})
+        search_result = json.loads(response.text)
+        # print(search_result)
+        # clean data
+        data = []
+        # 1. get actual cover image URL
+        for game in search_result:
+            row = {}
+            img_url = None
+            if 'name' in game:
+                row['name'] = game['name']
+            else:
+                continue
+
+            if 'cover' in game:
+                url = "https://api.igdb.com/v4/covers"
+                payload = "fields url; where id=%d;" % (game['cover'])
+                response = requests.request("POST", url, headers=headers, data=payload)
+                img_url = response.json()[0]["url"].split("//")[1]
+            row["img_url"] = img_url
+                
+        # 2. convert epoch to human readable form
+        
+            if 'first_release_date' in game:
+                row['release_date'] = datetime.fromtimestamp(game['first_release_date']).strftime("%Y-%m-%d")
+            else:
+                row['release_date'] = None
+        # 3. get actual genre strings
+            url = "https://api.igdb.com/v4/genres"
+            if 'genres' in game:
+                row['genres'] = []
+                for gid in game['genres']:
+                    payload = "fields name; where id=%d;"%gid
+                    response = requests.request("POST", url, headers=headers, data=payload)
+                    row['genres'].append(response.json()[0]["name"])
+
+
+            data.append(row)
+        print(data)
+        return render(request, 'search_result.html', {'games': data})
 
 
         
