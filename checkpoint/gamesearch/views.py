f075a8cb5c12bd790105a697351a777e5ddabb92
Anubhav Dinkar <ad6641@nyu.edu>
2024-10-17 12:41:52 -0400

    added search results page

--- checkpoint/gamesearch/views.py
+++ checkpoint/gamesearch/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-# import requests
-# import os
+import requests
+import os
+import json
 
 def search_game(request):
+    def authorize_igdb():
+        url = f"https://id.twitch.tv/oauth2/token?client_id={os.environ.get("client_id")}&client_secret={os.environ.get("client_secret")}&grant_type=client_credentials"
+        return requests.request("POST", url, headers={}, data={})
+
     if request.method == 'POST':
         game_query = request.POST.get('game_query')
-        print(game_query)
-        # Here you can call IGDB API using the game_query
-        # You can redirect to the results page or return the data
-        # return HttpResponseRedirect('/results/')
+        auth = authorize_igdb()
+        
+
+        url = "https://api.igdb.com/v4/games"
+
+        payload = "search \"%s\"; fields *;\n" % (game_query)
+        headers = {
+        'Client-ID': os.environ.get("client_id"),
+        'Authorization': f'Bearer {auth.json()["access_token"]}',
+        'Content-Type': 'text/plain',
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        search_result = response.json()
+        return render(request, 'search_result.html', {'games': search_result})
+
+
+        
     return render(request, 'search.html')
