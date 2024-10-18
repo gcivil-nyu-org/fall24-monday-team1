fbe789293a0d976885670e257dd30c4b8490663a
Anubhav Dinkar <ad6641@nyu.edu>
2024-10-18 12:39:40 -0400

    game details template ready; needs styling; image not fetching

--- checkpoint/gamesearch/urls.py
+++ checkpoint/gamesearch/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.search_game, name='search_game'), 
+    path('game/<int:game_id>/', views.game_details_view, name='game-details'),
+    path('game/<int:game_id>/fetch-data/', views.game_data_fetch_view, name='game-data-fetch'),
 ]
--- checkpoint/gamesearch/views.py
+++ checkpoint/gamesearch/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, JsonResponse
+
 import requests
 import os
 import json
 from datetime import datetime
+import time
 
-def search_game(request):
-    def authorize_igdb():
+def authorize_igdb():
         url = "https://id.twitch.tv/oauth2/token?client_id=%s&client_secret=%s&grant_type=client_credentials" % (os.environ['igdb_client_id'], os.environ['igdb_client_secret'])
-
         return requests.request("POST", url, headers={}, data={})
 
+def search_game(request):
     if request.method == 'POST':
         game_query = request.POST.get('game_query')
         auth = authorize_igdb()
@@ -26,9 +27,10 @@ def search_game(request):
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
-
-        search_result = json.loads(response.text)
-        # print(search_result)
+        
+        search_result = response.json()
+        
+        print(search_result)
         # clean data
         data = []
         # 1. get actual cover image URL
@@ -42,10 +44,13 @@ def search_game(request):
                 continue
 
             if 'cover' in game:
-                url = "https://api.igdb.com/v4/covers"
-                payload = "fields url; where id=%d;" % (game['cover'])
-                response = requests.request("POST", url, headers=headers, data=payload)
-                img_url = response.json()[0]["url"].split("//")[1]
+                try:
+                    url = "https://api.igdb.com/v4/covers"
+                    payload = "fields url; where id=%d;" % (game['cover'])
+                    response = requests.request("POST", url, headers=headers, data=payload)
+                    img_url = response.json()[0]["url"].split("//")[1]
+                except:
+                    pass
             row["img_url"] = img_url
                 
         # 2. convert epoch to human readable form
@@ -72,3 +77,47 @@ def search_game(request):
 
         
     return render(request, 'search.html')
+
+
+def game_details_view(request, game_id):
+    return render(request, 'game_details.html', {'game_id': game_id})
+
+def game_data_fetch_view(request, game_id):
+    auth = authorize_igdb()
+        
+
+    url = "https://api.igdb.com/v4/games"
+    
+    payload = "fields cover, genres, platforms, name, rating, first_release_date, summary, url; where id=%d;" % game_id
+    headers = {
+    'Client-ID': os.environ.get("igdb_client_id"),
+    'Authorization': f'Bearer {auth.json()["access_token"]}',
+    'Content-Type': 'text/plain',
+    }
+
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    data = {}
+    print(response.json())
+    
+    for key,value in response.json()[0].items():
+        if key == 'genres' or key == 'platforms':
+            data[key] = []
+            for id in value:
+                url = "https://api.igdb.com/v4/%s"%key
+                payload = "fields name; where id=%d;"%id
+                response = requests.request("POST", url, headers=headers, data=payload)
+                data[key].append(response.json()[0]["name"])
+        elif key == 'first_release_date':
+            data['release_year'] = datetime.fromtimestamp(value).strftime("%Y")
+        elif key == "cover":
+            url = "https://api.igdb.com/v4/covers"
+            payload = "fields url; where id=%d;" % (value)
+            print(payload)
+            response = requests.request("POST", url, headers=headers, data=payload)
+            data['cover'] = response.json()[0]["url"].split("//")[1]
+        else:
+            data[key] = value
+
+    # You can return HTML or JSON depending on how you want to populate the data
+    return JsonResponse(data)
