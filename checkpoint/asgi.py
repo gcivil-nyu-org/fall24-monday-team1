17198c5275b1b6b8ff1ed642b1b7ba2b07b4eade
Anubhav Dinkar <ad6641@nyu.edu>
2024-10-18 23:37:11 -0400

    added config files to root of repo; added ebignore file

--- checkpoint/asgi.py
+++ checkpoint/asgi.py
@@ -1,7 +1,7 @@
 """
 ASGI config for checkpoint project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named ````.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
