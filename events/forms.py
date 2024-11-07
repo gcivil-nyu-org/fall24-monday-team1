8876ff8870991272f7eacbd51bf8c85c6aac3f18
Zhuo ZENG <zzengan@connect.ust.hk>
2024-11-07 18:48:15 -0500

    remove forms

--- events/forms.py
+++ events/forms.py
@@ -1,19 +1,2 @@
 from django import forms
 from .models import Event
-
-class EventForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ['title', 'description', 'start_time', 'end_time', 'location']
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Event Title'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Event Description'}),
-            'start_time': forms.DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Start Time'}),
-            'end_time': forms.DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'End Time'}),
-            'location': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Location'}),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super(EventForm, self).__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.label = field.label.capitalize()  # Capitalize the field labels
