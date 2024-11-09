b0cb1c37d88c10a3c8cef43614b1cfbe9ee717f1
Anubhav Dinkar <ad6641@nyu.edu>
2024-11-09 12:34:50 -0500

    save to dynamo works

--- lists/urls.py
+++ lists/urls.py
@@ -7,4 +7,5 @@ app_name="lists"
 urlpatterns = [
     path('create-list/', views.create_list, name='create_list'),
     path('search-igdb-games/', views.search_igdb_games, name='search_igdb_games'),
+    path('save_list/', views.save_list, name='save_list'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,16 @@
+import urllib.parse
 from django.shortcuts import render
 import requests
 from django.http import JsonResponse
 import os
 from gamesearch.views import authorize_igdb
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+import json
+import boto3
+import urllib
 
+@login_required
 def create_list(request):
     return render(request, 'create_list.html')
 
@@ -24,3 +30,43 @@ def search_igdb_games(request):
     response = requests.post("https://api.igdb.com/v4/games", headers=headers, data=payload)
     games = response.json()
     return JsonResponse(games, safe=False)
+
+
+
+@login_required
+@csrf_exempt
+def save_list(request):
+    dynamodb = boto3.resource(
+        'dynamodb',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+        region_name='us-east-1'
+    )
+    table = dynamodb.Table('lists')
+
+    if request.method == 'POST':
+        try:
+            data = dict(request.POST)
+            print(data)
+            username = request.user.username  # Get the current user's username
+
+            # Prepare the item to be saved in DynamoDB
+            item = {
+                'username': username,
+                'name': data['name'],
+                'description': data['description'],
+                'visibility': data['visibility'],
+                'games': data['games[]']
+            }
+            
+            try:
+                table.put_item(Item=item)
+            except Exception as e:
+                print(e)
+
+            return JsonResponse({"message": "List saved successfully!"}, status=200)
+        
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    else:
+        return JsonResponse({"error": "Invalid request method."}, status=400)
