6901f7f3dd74750ebb5e8e5f36e7536e26971580
Anubhav Dinkar <ad6641@nyu.edu>
2024-11-09 09:28:34 -0500

    populating the search dropdown works

--- lists/views.py
+++ lists/views.py
@@ -3,19 +3,22 @@ import requests
 from django.http import JsonResponse
 import os
 from gamesearch.views import authorize_igdb
-
+from django.views.decorators.csrf import csrf_exempt
 
 def create_list(request):
     return render(request, 'create_list.html')
 
+@csrf_exempt
 def search_igdb_games(request):
-    query = request.GET.get('query', '')
+    query = request.POST.get('query', '')
     if not query:
         return JsonResponse([], safe=False)
 
     auth = authorize_igdb()
     headers = {
-        'Authorization': f'Bearer {auth.json()["access_token"]}'
+        'Client-ID': os.environ.get("igdb_client_id"),
+        'Authorization': f'Bearer {auth.json()["access_token"]}',
+        'Content-Type': 'text/plain',
     }
     payload = f'search "{query}"; fields name;'
     response = requests.post("https://api.igdb.com/v4/games", headers=headers, data=payload)
